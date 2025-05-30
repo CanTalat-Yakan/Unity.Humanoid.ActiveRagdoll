5e1ab8aa7aec314de0a9d1c16e15d2f5ee8a2c5d
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-30 17:22:11 +0200

    ⠀⠀⠀⠀⠀

---
+++ Editor/HumanoidSetUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+public class HumanoidSetUp : MonoBehaviour
+{
+    [Tooltip("Static animator hips.")]
+    public Transform masterRoot;
+    [Tooltip("Ragdoll hips.")]
+    public Transform slaveRoot;
+    [Tooltip("Ragdoll looses strength when colliding with other objects except for objects with layers contained in this mask.")]
+    public LayerMask dontLooseStrengthLayerMask;
+
+
+    public SlaveController slaveController;
+    public AnimationFollowing animFollow;
+}
